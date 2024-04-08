74f38daef911ab3ef433813261a701557ea4484f
vanebrina <seider.sanin22135@ucaldas.edu.co>
2024-04-08 08:10:06 -0500

    conection to Data Base

---
+++ index.js
@@ -0,0 +1,38 @@
+// Importacin del franwork expres para realizar solicitud http
+const express = require('express');
+const mongoose = require('mongoose');
+//const cors = require('cors');
+//bodyparser es un middleware de express
+//que parsea el cuerpo de las solicitudes entrantes
+//en formato JSON, multipartes y lo convierte en un objeto java Script
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//poder accerder a leer eel archivo .env
+require("dotenv").config();
+const PORT = process.env.PORT || 3005;
+
+app.listen(PORT, () => {
+    console.log(`server runing on port ${PORT}`);
+    mongo_connect();
+});
+
+const mongo_connect = () => {
+    try {
+        mongoose
+            .connect(process.env.DATABASE_CONNECTION_STRING)
+            .then(() => {
+                console.log("Connected to MongoDB");
+            })
+            .catch ((err) => {
+                console.log("Connected error in the bd" + ' ' + err.message);
+            });
+    } catch (err) {
+        console.log(err);
+    };
+};
+
+//rutas
